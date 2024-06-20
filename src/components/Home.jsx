562aad96f3212c08d0348ef4b182ff8f0a347948
Nilogrib <142620260+Nilogrib@users.noreply.github.com>
2024-06-20 19:02:21 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+
 import pic from "../../public/photo.jpg";
-import {
-  FaSquareInstagram,
-  FaXTwitter,
-  FaGithub,
-} from "react-icons/fa6";
+
+import { FaSquareFacebook, FaSquareInstagram, FaTwitter, FaX, FaXTwitter, FaGithub } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
-import { SiMongodb, SiExpress } from "react-icons/si";
-import { FaReact, FaNodeJs } from "react-icons/fa6";
-import ReactTyped from "react-typed";
+
+import { SiMongodb } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa6";
+
+import { ReactTyped } from "react-typed";
 
 function Home() {
   return (
@@ -17,10 +19,14 @@ function Home() {
         name="Home"
         className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-28"
       >
-        <div className="flex flex-col md:flex-row items-center">
-          <div className="md:w-1/2 order-2 md:order-1 mt-12 md:mt-0 space-y-6 md:space-y-8">
-            <div className="flex space-x-2 text-2xl md:text-4xl">
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1">
+          <br />
+          <br />
+          <br />
+            <div className="flex space-x-1 text-2xl md:text-4xl">
               <h1>Hello, I'm a</h1>
+              {/* <span >Developer</span> */}
               <ReactTyped
                 className="text-red-700 font-bold"
                 strings={["Developer", "Programmer", "Coder"]}
@@ -29,38 +35,41 @@ function Home() {
                 loop={true}
               />
             </div>
+            <br />
             <p className="text-sm md:text-md text-justify">
-              I am a skilled web developer and competitive programmer with strong proficiency in Data Structures and Algorithms (DSA). My expertise includes building robust web applications and solving complex programming challenges. Passionate about coding and continuously improving my skills, I thrive in fast-paced, problem-solving environments.
+            I am a skilled web developer and competitive programmer with strong proficiency in Data Structures and Algorithms (DSA). My expertise includes building robust web applications and solving complex programming challenges. Passionate about coding and continuously improving my skills, I thrive in fast-paced, problem-solving environments.
             </p>
-            <div className="flex flex-col md:flex-row justify-between space-y-6 md:space-y-0">
-              <div className="space-y-2">
-                <h1 className="font-bold text-center">Available on</h1>
-                <ul className="flex space-x-5 justify-center">
+            <br />
+            {/* social media icons */}
+            <div className="flex flex-col items-center md:flex-row justify-between space-y-6 md:space-y-0">
+              <div className="  space-y-2">
+                <h1 className="font-bold text-center ">Available on</h1>
+                <ul className="flex space-x-5">
                   <li>
-                    <a href="https://www.instagram.com/nilogrib.13/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.instagram.com/nilogrib.13/" target="_blank">
                       <FaSquareInstagram className="text-2xl cursor-pointer" />
                     </a>
                   </li>
                   <li>
-                    <a href="https://www.linkedin.com/in/nilogrib/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.linkedin.com/in/nilogrib/" target="_blank">
                       <FaLinkedin className="text-2xl cursor-pointer" />
                     </a>
                   </li>
                   <li>
-                    <a href="https://x.com/nilogrib" target="_blank" rel="noopener noreferrer">
+                    <a href="https://x.com/nilogrib" target="_blank">
                       <FaXTwitter className="text-2xl cursor-pointer" />
                     </a>
                   </li>
                   <li>
-                    <a href="https://github.com/Nilogrib" target="_blank" rel="noopener noreferrer">
+                    <a href="https://github.com/Nilogrib" target="_blank">
                       <FaGithub className="text-2xl cursor-pointer" />
                     </a>
                   </li>
                 </ul>
               </div>
-              <div className="space-y-2">
+              <div className=" space-y-2">
                 <h1 className="font-bold text-center">Currently working on</h1>
-                <div className="flex space-x-5 justify-center">
+                <div className="flex space-x-5">
                   <SiMongodb className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
                   <SiExpress className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
                   <FaReact className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
@@ -69,15 +78,16 @@ function Home() {
               </div>
             </div>
           </div>
-          <div className="md:w-1/2 order-1 md:order-2 flex justify-center mb-12 md:mb-0">
+          <div className="md:w-1/2 md:ml-48 md:mt-20 mt-8 order-1">
             <img
               src={pic}
-              className="rounded-full md:w-[400px] md:h-[500px] w-[300px] h-[300px]"
-              alt="Profile"
+              className="rounded-full md:w-[400px] md:h-[500px]"
+              alt=""
             />
           </div>
         </div>
       </div>
+
       <hr />
     </>
   );
